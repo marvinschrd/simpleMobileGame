ee533208de3c18d9db57b3452de507e9836bf5e1
Marvin <marvin.k.scharding@gmail.com>
2021-01-24 19:14:39 +0100

    Started

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]private GameObject normalBlock;
+    class BlockSpawn
+    {
+        public Vector2 position;
+        private int type;
+        private int hitPoints;
+    }
+
+    private List<BlockSpawn> _blockSpawns = new List<BlockSpawn>();
+
+    private List<GameObject> blocks = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xPosition = 1;
+        for (int i = 0; i <= 6; i++)
+        {
+            BlockSpawn spawn = new BlockSpawn();
+            spawn.position = new Vector2(xPosition, 9.5f);
+            _blockSpawns.Add(spawn);
+            xPosition += 1.6f;
+        }
+        
+        SpawnBlocks();
+    }
+    
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MoveBlock();
+            SpawnBlocks();
+        }
+    }
+
+
+
+    void SpawnBlocks()
+    {
+        foreach (BlockSpawn spawn in _blockSpawns)
+        {
+            GameObject newBlock;
+            newBlock = Instantiate(normalBlock, spawn.position, quaternion.identity);
+            blocks.Add(newBlock);
+        }
+    }
+
+    void MoveBlock()
+    {
+        foreach ( GameObject block in blocks)
+        {
+            block.transform.position = new Vector2(block.transform.position.x, block.transform.position.y - 1);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] int rows;
+    [SerializeField] int cols;
+    private float tileSize;
+
+    [SerializeField] private float cellSize;
+    private Vector3 position;
+
+
+    private int[,] gridArray;
+
+    struct Cell
+    {
+        private Vector2 position;
+    }
+
+    private Cell[,] cells;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i <= gridArray.Length; i++)
+        {
+            for (int y = 0; y <= gridArray.Length; y++)
+            { 
+                
+                Gizmos.DrawCube( new Vector3(1,1,1),new Vector3(1,1,0));
+            }
+        }
+    }
+}
